8b3e42ae6a93f73afb352bef43dcc154dc52dd71
Alan Tao <alantao@Alans-MacBook-Pro-2.local>
2025-03-01 03:22:54 -0600

    uh

--- backend/api.py
+++ backend/api.py
@@ -1,28 +1,62 @@
-from fastapi import FastAPI
-import spoonacular as sp
+from fastapi import FastAPI, HTTPException
+import requests
 import pprint
-import json
 
 app = FastAPI()
-api = sp.API("4136cd9fa253479aaeb846b336d6dd31")
+API_KEY = "4136cd9fa253479aaeb846b336d6dd31"
+BASE_URL = "https://api.spoonacular.com"
+
+cached_recipes = {}
+
+@app.get("/")
+async def root():
+    try:
+        print("Fetching sample recipes = ITALIAN RECIPES UNDER 10 DOLLARS")
+        budget = 1000.0
+        cuisine = "italian"
+        recipes = await get_recipes(budget, cuisine)
+        return {"message": "Hello World", "recipes": recipes["results"]}
+    except Exception as e:
+        return {"error": str(e)}
 
 
 @app.get("/recipes")
-async def get_recipes(budget: float = 10.0, cuisine: str = ""):
+async def get_recipes(budget: float, cuisine: str):
+    try: 
+        print("Making request to API...")
+        search_params = {
+            "cuisine": cuisine,
+            "number": 100,
+            "apiKey": API_KEY
+        }
 
-    response = api.search_recipes_complex(cuisine )
-    response_dict = json.load(response)
+        response = requests.get(f"{BASE_URL}/recipes/complexSearch", params=search_params)
+        data = response.json()
 
-    filtered_response = filter(lambda x: x. <= budget, response_dict)
-    # data = response.json()
+        if "results" not in data:
+            print('failed')
+            raise HTTPException(status_code = 404, detail = "No Recipes Found")
+        
+        valid_recipes = []
+        for recipe in data['results']:
+            cost = 0.0
+            if recipe['id'] in cached_recipes:
+                cost = cached_recipes[recipe['id']]
+            else:
+                price_params = {
+                    "apiKey": API_KEY
+                }
+                breakdown_response = requests.get(f"{BASE_URL}/recipes/{recipe['id']}/priceBreakdownWidget.json", params=price_params)
+                breakdown_data = breakdown_response.json()
 
-    print(data)
-    return data
+                cost = breakdown_data['totalCostPerServing'] / 100
+                cached_recipes[recipe['id']] = cost
+            if cost <= budget:
+                valid_recipes.append({"id": recipe['id'], "recipeName": recipe['title'], "cost": cost})
 
-@app.post("/history")
-async def get_history():
-    query = """SELECT User, """
+            print(valid_recipes)
 
-
-
+        return {"results": valid_recipes}
+    except Exception as e:
+        raise HTTPException(status_code = 500, detail = str(e))
 
