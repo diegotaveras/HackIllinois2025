4116855c995d40ea85145d00936b04f6e09909fa
yvk13 <yuktha.kakolla13@gmail.com>
2025-03-01 16:24:28 -0600

    previous code

--- frontend/hackillinois2025/src/App.tsx
+++ frontend/hackillinois2025/src/App.tsx
@@ -1,31 +1,45 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import MenubarDemo from "./Menu"
-import {InputField} from "./Input"
-import {ScrollAreaSection} from "./Scroll-area"
-import { Separator } from "./components/ui/separator"
-import History from "./pages/history";
+import { useEffect, useState } from "react";
+import NavBar from "./navbar";
+// import "./App.css";
+import { Recipetype } from "./types";
+import {RecipeList} from "./recipe";
+import {MenuBar} from "./menu"
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 
 export default function App() {
 
   return (
-    <div>
+    <div className="flex flex-col h-full w-full">
+      <div className="flex">
+        <MenuBar />
+      </div>
+      <div style={{padding: 70}}></div>
       <div>
-        <MenubarDemo />
-      </div>
-      <div className="container bg-blue-500 w-full h-27">
-        <h1>RecipeApp</h1>
-        <p>Budget your meals in college</p>
-      </div>
-      <div style={{ padding: '50px'}}>
-      </div>
-      <InputField />
-      <Separator className="my-2" />
-      <div>
-        <ScrollAreaSection />
+        <div className=""><NavBar onSearch={search}></NavBar></div>
+        <div>
+            {searchResults.length === 0 ? (
+              <p>No recipes found.</p>
+            ) : (
+              <RecipeList recipes={searchResults} />
+            )}
+        </div>
       </div>
     </div>
-  );
+
+    //     <main>
+    //   <BrowserRouter>
+    //     <Navbar />
+    //     <div className='min-h-screen w-full flex items-center justify-center'>
+    //       <Routes>
+    //         <Route path='/' element={<Home />} />
+    //         <Route path='/about' element={<About />} />
+    //         <Route path='/pricing' element={<Pricing />} />
+    //         <Route path='/contact' element={<Contact />} />
+    //       </Routes>
+    //     </div>
+    //   </BrowserRouter>
+    // </main>
+    
+  )
 }
