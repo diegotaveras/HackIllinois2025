df8db8ff01665032e0be42b34391f2c1b262aa79
Alan Tao <alantao@Alans-MacBook-Pro-2.local>
2025-03-01 08:00:48 -0600

    bleh

--- backend/api.py
+++ backend/api.py
@@ -2,11 +2,73 @@ from fastapi import FastAPI, HTTPException
 import requests
 import pprint
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+# Allow frontend on Vite's default port (5173)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # Add your frontend's port
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 API_KEY = "4136cd9fa253479aaeb846b336d6dd31"
 BASE_URL = "https://api.spoonacular.com"
 
 cached_recipes = {}
+
+MOCK_COMPLEX_SEARCH = {
+    "results": [
+        {
+            "id": 1234,
+            "title": "Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs",
+            "cost": 10
+        },
+        {
+            "id": 5678,
+            "title": "What to make for dinner tonight?? Bruschetta Style Pork & Pasta",
+            "cost": 10
+        }
+    ],
+}
+
+MOCK_PRICES_BREAKDOWN = {
+    "ingredients": [
+        {
+            "amount": {
+                "metric": {
+                    "unit": "g",
+                    "value": 222.0
+                },
+                "us": {
+                    "unit": "cups",
+                    "value": 1.5
+                }
+            },
+            "name": "blueberries",
+            "price": 174.43
+        },
+        {
+            "amount": {
+                "metric": {
+                    "unit": "",
+                    "value": 1.0
+                },
+                "us": {
+                    "unit": "",
+                    "value": 1.0
+                }
+            },
+            "name": "egg white",
+            "price": 18.21
+        }
+    ],
+    "totalCost": 1500,
+    "totalCostPerServing": 750
+}
 
 @app.get("/")
 async def root():
@@ -22,41 +84,45 @@ async def root():
 
 @app.get("/recipes")
 async def get_recipes(budget: float, cuisine: str):
-    try: 
-        print("Making request to API...")
-        search_params = {
-            "cuisine": cuisine,
-            "number": 100,
-            "apiKey": API_KEY
-        }
+    return {"results": MOCK_COMPLEX_SEARCH['results']}
+    
 
-        response = requests.get(f"{BASE_URL}/recipes/complexSearch", params=search_params)
-        data = response.json()
+# async def get_recipes(budget: float, cuisine: str):
+#     try: 
+#         print("Making request to API...")
+#         search_params = {
+#             "cuisine": cuisine,
+#             "number": 100,
+#             "apiKey": API_KEY
+#         }
 
-        if "results" not in data:
-            print('failed')
-            raise HTTPException(status_code = 404, detail = "No Recipes Found")
+#         response = requests.get(f"{BASE_URL}/recipes/complexSearch", params=search_params)
+#         data = response.json()
+
+#         if "results" not in data:
+#             print('failed')
+#             raise HTTPException(status_code = 404, detail = "No Recipes Found")
         
-        valid_recipes = []
-        for recipe in data['results']:
-            cost = 0.0
-            if recipe['id'] in cached_recipes:
-                cost = cached_recipes[recipe['id']]
-            else:
-                price_params = {
-                    "apiKey": API_KEY
-                }
-                breakdown_response = requests.get(f"{BASE_URL}/recipes/{recipe['id']}/priceBreakdownWidget.json", params=price_params)
-                breakdown_data = breakdown_response.json()
+#         valid_recipes = []
+#         for recipe in data['results']:
+#             cost = 0.0
+#             if recipe['id'] in cached_recipes:
+#                 cost = cached_recipes[recipe['id']]
+#             else:
+#                 price_params = {
+#                     "apiKey": API_KEY
+#                 }
+#                 breakdown_response = requests.get(f"{BASE_URL}/recipes/{recipe['id']}/priceBreakdownWidget.json", params=price_params)
+#                 breakdown_data = breakdown_response.json()
 
-                cost = breakdown_data['totalCostPerServing'] / 100
-                cached_recipes[recipe['id']] = cost
-            if cost <= budget:
-                valid_recipes.append({"id": recipe['id'], "recipeName": recipe['title'], "cost": cost})
+#                 cost = breakdown_data['totalCostPerServing'] / 100
+#                 cached_recipes[recipe['id']] = cost
+#             if cost <= budget:
+#                 valid_recipes.append({"id": recipe['id'], "recipeName": recipe['title'], "cost": cost})
 
-            print(valid_recipes)
+#             print(valid_recipes)
 
-        return {"results": valid_recipes}
-    except Exception as e:
-        raise HTTPException(status_code = 500, detail = str(e))
+#         return {"results": valid_recipes}
+#     except Exception as e:
+#         raise HTTPException(status_code = 500, detail = str(e))
 
--- frontend/hackillinois2025/src/App.tsx
+++ frontend/hackillinois2025/src/App.tsx
@@ -1,31 +1,43 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
+import NavBar from "./navbar";
 import "./App.css";
+import { Recipetype } from "./types";
+import {RecipeList} from "./recipe";
+
 
 export default function App() {
-  const [count, setCount] = useState(0);
+  const [searchResults, setSearchResults] = useState<Recipetype[]>([])
+
+  useEffect(() => {
+    console.log(searchResults); // Access the updated value here
+  }, [searchResults]);
+
+  const search = async(cuisine: string, budget: number) => {
+    try {
+      const response = await fetch(`http://127.0.0.1:8000/recipes?budget=${budget}&cuisine=${cuisine}`)
+      const data = await response.json()
+      const results: Recipetype[] = data.results
+      if (results.length > 0) {
+        setSearchResults(results)
+      } 
+    } catch(error) {
+      console.log("Error:", error)
+    }
+  }
+
+
 
   return (
     <>
+      <div><NavBar onSearch={search}></NavBar></div>
       <div>
-        <h>What's your budget?</h>
-        <div>
-          <div></div>
-        </div>
+          {searchResults.length === 0 ? (
+            <p>No recipes found.</p>
+          ) : (
+            <RecipeList recipes={searchResults} />
+          )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
+    
   );
 }
