ceb71d05010069f13e8bf5eaf6890290f7f8e926
Tristan Tran <149007776+TristanTran23@users.noreply.github.com>
2025-03-01 21:27:47 +0000

    finished history page outline

--- frontend/hackillinois2025/src/App.tsx
+++ frontend/hackillinois2025/src/App.tsx
@@ -1,31 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import MenubarDemo from "./Menu"
-import {InputField} from "./Input"
-import {ScrollAreaSection} from "./Scroll-area"
-import { Separator } from "./components/ui/separator"
-import History from "./pages/history";
+import { Routes, Route } from "react-router-dom";
+import MenubarDemo from "./Menu";
+import Home from "./pages/Home";
+import History from "./pages/History";
 
 export default function App() {
-
   return (
     <div>
-      <div>
-        <MenubarDemo />
-      </div>
-      <div className="container bg-blue-500 w-full h-27">
-        <h1>RecipeApp</h1>
-        <p>Budget your meals in college</p>
-      </div>
-      <div style={{ padding: '50px'}}>
-      </div>
-      <InputField />
-      <Separator className="my-2" />
-      <div>
-        <ScrollAreaSection />
-      </div>
+      <MenubarDemo />
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Routes>
     </div>
   );
 }
--- frontend/hackillinois2025/src/pages/History.tsx
+++ frontend/hackillinois2025/src/pages/History.tsx
@@ -2,18 +2,25 @@ import React, {useState} from "react";
 import Component from "../components/chart";
 
 export default function History() {
-  const [history,setHistory] = useState<{name: string}[]>([])
+  // const [history,setHistory] = useState<{name: string}[]>([])
+  const history = [
+    {name: "Breakfast"},
+    {name: "Lunch"},
+    {name: "Dinner"},
+    {name: "Snack"},
+    {name: "Dessert"},
+  ]
   
   const HistoryItem = ({name, index}: {name:string;index:number}) => (
     <div className="flex flex-row items-center bg-grey p-2">
       <p>{index}</p>
-      <p>{name}&& </p>
+      <p className="pl-2">{name} </p>
     </div>
   )
   
   return(
-    <div className="flex flex-row justify-between justify-items-stretch">
-      <div>
+    <div className="w-full flex flex-row justify-between justify-items-center">
+      <div className="flex flex-col justify-center justify-content-center">
         <p className="text-3xl">Meal History</p>
           {history.map((item, index) => (
             <HistoryItem
@@ -22,8 +29,8 @@ export default function History() {
             />
           ))}
       </div>
-      <div className="flex flex-col justify-center justify-items-center">
-        <p className="text-3xl">Weekly</p>
+      <div className="flex flex-col justify-center justify-content-center">
+        <p className="flex text-3xl justify-center align-center">Weekly</p>
         <Component />
       </div>
     </div>
